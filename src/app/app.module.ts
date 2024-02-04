a4581aac847c74ca93b3700b738662295838eb2b
EduardoCampo <eduardocampo91@gmail.com>
2024-02-03 23:30:39 -0500

    assignament for reverse and sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
 import { FilterPipe } from './filter.pipe';
 import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
 
 @NgModule({
   declarations: [
     AppComponent, 
     ShortenPipe, 
-    FilterPipe, ReversePipe
+    FilterPipe, ReversePipe, SortPipe
   ],
   imports: [
     BrowserModule,
